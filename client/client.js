732501ec9207722e62ef19f78e0ead49ed948515
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-05-23 01:23:08 +0200

    add: range check

--- client/client.js
+++ client/client.js
@@ -1,2 +1,3 @@
 /// <reference types="@altv/types-client" />
-import * as alt from 'alt-client'
+import * as alt from 'alt-client';
+import * as native from 'natives';
--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 /// <reference types="@altv/types-server" />
-import * as alt from 'alt-server'
+import * as alt from 'alt-server';
 
-// create npc (in gta its called Ped) helper func
-function createPed(model, x, y, z, heading) {
-    const ped = new alt.Ped(model, new alt.Vector3(x, y, z), new alt.Vector3(0, 0, heading));
-    return ped;
-  }
-  
-  function loadSprayNpc() {
-    const ped1 = createPed('u_m_m_streetart_01', 1, 1, 71, 0);
-  }
-  
-  loadSprayNpc(); //load npc
+// helper for range https://docs.altv.mp/js/articles/snippets/math.html
+function distance2d(vector1, vector2) {
+    if (vector1 === undefined || vector2 === undefined) {
+        throw new Error('distance2d => vector1 or vector2 is undefined');
+    }
+
+    return Math.sqrt(Math.pow(vector1.x - vector2.x, 2) + Math.pow(vector1.y - vector2.y, 2));
+}
+
+const npcPosition = new alt.Vector3(1, 1, 71);
+const interactionDistance = 5;
+const npc = new alt.Ped('u_m_m_streetart_01', npcPosition, 0);
+
+alt.everyTick(() => {
+    alt.Player.all.forEach((player) => {
+        const playerPosition = player.pos;
+        const distanceToNPC = distance2d(playerPosition, npc.pos);
+        if (distanceToNPC < interactionDistance) {
+            alt.emitClient(player, 'playerEnteredNPCRange');
+            alt.log(`${player.name} entered NPC range`);
+        } else {
+            alt.emitClient(player, 'playerLeftNPCRange');
+            alt.log(`${player.name} left NPC range`);
+        }
+    });
+});
