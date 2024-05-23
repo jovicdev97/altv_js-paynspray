f1b128c0df5fe2f2fa9ea111e54af67e3899826e
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-05-23 12:17:23 +0200

    add: notification when entering ped range

--- client/client.js
+++ client/client.js
@@ -1,3 +1,34 @@
 /// <reference types="@altv/types-client" />
 import * as alt from 'alt-client';
 import * as native from 'natives';
+
+let isNearNPC = false;
+
+alt.onServer('playerEnteredNPCRange', () => {
+    if (!isNearNPC) {
+        isNearNPC = true;
+        showNotification('CHAR_DEFAULT', 'Info', '', 'Du bist in der Nähe des NPCs');
+    }
+});
+
+alt.onServer('playerLeftNPCRange', () => {
+    if (isNearNPC) {
+        isNearNPC = false;
+    }
+});
+
+function showNotification(imageName, headerMsg, detailsMsg, message) {
+    native.beginTextCommandThefeedPost('STRING');
+    native.addTextComponentSubstringPlayerName(message);
+    native.endTextCommandThefeedPostMessagetextTu(
+        imageName.toUpperCase(),
+        imageName.toUpperCase(),
+        false,
+        4,
+        headerMsg,
+        detailsMsg,
+        1.0,
+        ''
+    );
+    native.endTextCommandThefeedPostTicker(false, false);
+}
