da5cf845c463286fbf7deef528218244cd027d7d
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-05-24 15:38:16 +0200

    add: colshape

--- client/client.js
+++ client/client.js
@@ -2,21 +2,6 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 
-let isNearNPC = false;
-
-alt.onServer('playerEnteredNPCRange', () => {
-    if (!isNearNPC) {
-        isNearNPC = true;
-        showNotification('CHAR_DEFAULT', 'Info', '', 'Du bist in der Nähe des NPCs');
-    }
-});
-
-alt.onServer('playerLeftNPCRange', () => {
-    if (isNearNPC) {
-        isNearNPC = false;
-    }
-});
-
 function showNotification(imageName, headerMsg, detailsMsg, message) {
     native.beginTextCommandThefeedPost('STRING');
     native.addTextComponentSubstringPlayerName(message);
@@ -32,3 +17,11 @@ function showNotification(imageName, headerMsg, detailsMsg, message) {
     );
     native.endTextCommandThefeedPostTicker(false, false);
 }
+
+alt.onServer('entityEnterColshape', () => {
+    showNotification('CHAR_DEFAULT', 'Info', '', 'Jää ... komm näher ...');
+});
+
+alt.onServer('entityLeaveColshape', () => {
+    showNotification('CHAR_DEFAULT', 'Info', '', 'Jää ... Hau rein ...');
+});
--- server/server.js
+++ server/server.js
@@ -1,29 +1,24 @@
 /// <reference types="@altv/types-server" />
 import * as alt from 'alt-server';
 
-// helper for range https://docs.altv.mp/js/articles/snippets/math.html
-function distance2d(vector1, vector2) {
-    if (vector1 === undefined || vector2 === undefined) {
-        throw new Error('distance2d => vector1 or vector2 is undefined');
-    }
-
-    return Math.sqrt(Math.pow(vector1.x - vector2.x, 2) + Math.pow(vector1.y - vector2.y, 2));
-}
-
 const npcPosition = new alt.Vector3(1, 1, 71);
-const interactionDistance = 5;
 const npc = new alt.Ped('u_m_m_streetart_01', npcPosition, 0);
+npc.frozen = true;
+npc.health = 8191;
+npc.maxHealth = 8191;
+//todo: make npc invinc
 
-alt.everyTick(() => {
-    alt.Player.all.forEach((player) => {
-        const playerPosition = player.pos;
-        const distanceToNPC = distance2d(playerPosition, npc.pos);
-        if (distanceToNPC < interactionDistance) {
-            alt.emitClient(player, 'playerEnteredNPCRange');
-            alt.log(`${player.name} entered NPC range`);
-        } else {
-            alt.emitClient(player, 'playerLeftNPCRange');
-            alt.log(`${player.name} left NPC range`);
-        }
-    });
+const colShape = new alt.ColshapeCircle(npcPosition.x, npcPosition.y, 3);
+npc.setMeta('colshape', colShape);
+
+alt.on('entityEnterColshape', (colshape, entity) => {
+    if (entity instanceof alt.Player && colshape === colShape) {
+        alt.emitClient(entity, 'entityEnterColshape');
+    }
+});
+
+alt.on('entityLeaveColshape', (colshape, entity) => {
+    if (entity instanceof alt.Player && colshape === colShape) {
+        alt.emitClient(entity, 'entityLeaveColshape');
+    }
 });
