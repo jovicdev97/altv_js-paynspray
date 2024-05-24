065dbf481d4ccd456da5510b0b80f6351ebd02d8
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-05-24 21:28:55 +0200

    fix: event stacking such as when the player press multiple times e

--- server/server.js
+++ server/server.js
@@ -26,28 +26,47 @@ try {
 
         alt.onClient('playerPressedButtonE', async (player) => {
             try {
+                const hasPainted = player.getMeta('hasPainted');
+                if (hasPainted) {
+                    alt.emitClient(player, 'notify');
+                    return;
+                }
                 const vehicle = player.vehicle;
                 if (!vehicle) return;
-                alt.emitClient(player, 'wirdGesprayed'); // for Notification
-                alt.on('playerLeftVehicle', (player) => { // we want player to leave vehicle
-                    vehicle.frozen = true;
-                    vehicle.engineOn = false;
-                    alt.setTimeout(() => {
-                        vehicle.numberPlateText = generateRandomString();
-                        vehicle.primaryColor = generateRandomNumberBetween1and159();
-                        vehicle.secondaryColor = generateRandomNumberBetween1and159();
-                        vehicle.dirtLevel = 15; // will make the car very dirty after work for police rp
-                        vehicle.frozen = false;
-                    }, 10000); // debugging: should change to notification value
-                });
+    
+                player.setMeta('hasPainted', true);                // add meta to player
+                alt.emitClient(player, 'wirdGesprayed');           // for Notification
+
+                /* 
+                # Add the listener as a arrow function 
+                # This will help to avoid event stacking such as when the player hit multiple times E
+                # and the event will be called multiple times
+                */
+
+                const handlePlayerLeftVehicle = (playerLeavingVehicle) => {
+                    if (playerLeavingVehicle === player) {
+                        vehicle.frozen = true;
+                        vehicle.engineOn = false;
+                        vehicle.lockState = 2;
+                        alt.setTimeout(() => {
+                            vehicle.numberPlateText = generateRandomString();
+                            vehicle.primaryColor = generateRandomNumberBetween1and159();
+                            vehicle.secondaryColor = generateRandomNumberBetween1and159();
+                            vehicle.dirtLevel = 15; // will make the car very dirty after work for police rp
+                            vehicle.frozen = false;
+                            vehicle.lockState = 1;
+                        }, 10000); // debugging: should change to notification value
+                        alt.off('playerLeftVehicle', handlePlayerLeftVehicle); // remove the listener to avoid event stacking
+                    }
+                };
+                alt.on('playerLeftVehicle', handlePlayerLeftVehicle);
             } catch (error) {
                 console.error(error);
             }
         });
-        
     } else {
-        console.error("error spawn");
+        console.error("error");
     }
 } catch (error) {
-    console.error("error:", error);
+    console.error(error);
 }
