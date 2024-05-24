b0545fb38d243c391429a93f128a054ff73c2e35
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-05-24 16:55:18 +0200

    add: helpers

---
+++ helper/helper.js
@@ -0,0 +1,15 @@
+export function generateRandomString() { // number plate
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const minLength = 5;
+    const maxLength = 7;
+    const length = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+export function generateRandomNumberBetween1and159() { // vehicle color
+    return Math.floor(Math.random() * 159) + 1;
+}
