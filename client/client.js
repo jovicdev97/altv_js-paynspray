76b28f6a2abeaf3d82bee6f5886c82cbbbb09566
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-06-05 16:18:51 +0200

    add: audio logic for client

--- client/client.js
+++ client/client.js
@@ -2,51 +2,45 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 
-// helper function to show notification
-function showNotification(imageName, headerMsg, detailsMsg, message) {
-  native.beginTextCommandThefeedPost('STRING');
-  native.addTextComponentSubstringPlayerName(message);
-  native.endTextCommandThefeedPostMessagetextTu(
-      imageName.toUpperCase(),
-      imageName.toUpperCase(),
-      false,
-      4,
-      headerMsg,
-      detailsMsg,
-      1.0,
-      ''
-  );
-  native.endTextCommandThefeedPostTicker(false, false);
+let webview = null;
+let audio = null;
+let isInColshape = false;
+
+function playLocalSound(filePath, volume = 1) {
+    if (audio) {
+        audio.destroy();
+    }
+    audio = new alt.Audio(filePath);
+    audio.volume = volume;
+    audio.addOutput(new alt.AudioOutputFrontend());
+    audio.play();
 }
 
-// colshape
-let isInColshape = false;
+
+
 
 alt.onServer('entityEnterColshape', () => {
     showNotification('CHAR_DEFAULT', 'Info', '', 'Press E to change your vehicle color and numberplate');
     isInColshape = true;
 });
+
 alt.onServer('entityLeaveColshape', () => {
     isInColshape = false;
 });
 
-// key press from player
-alt.on('keydown', (key) => {
-    if (key === 69 && isInColshape) { 
-        alt.emitServer('playerPressedButtonE');
-    }
-});
-
-// spray notifications
-alt.onServer('wirdGesprayed', (player) => {
-    showNotification('CHAR_DEFAULT', 'Info', '', 'This will take 20s');
-  })
-
 alt.onServer('notify', () => {
-    showNotification('CHAR_DEFAULT', 'Info', '', 'Bro .. back off !!');
+    showNotification('CHAR_DEFAULT', 'Info', '', 'This car is to hot for the mechanic. You should leave this place.');
+    playLocalSound('/client/assets/wav/go_away.mp3');
 });
 
-// edit spray npc
+alt.onServer('startwork', () => {
+    playLocalSound('/client/assets/wav/get_to_spray.mp3'); 
+});
+
+alt.onServer('finishcar', () => {
+    playLocalSound('/client/assets/wav/rdy_car.mp3'); 
+});
+
 alt.on('gameEntityCreate', (ped) => {
     if (!(ped instanceof alt.Ped)) return
     alt.setTimeout(() => {
@@ -56,10 +50,10 @@ alt.on('gameEntityCreate', (ped) => {
         native.taskSetBlockingOfNonTemporaryEvents(ped, true);
         native.setEntityProofs(ped, true, true, true, true, true, true, true, true);
         native.setPedTreatedAsFriendly(ped, 1, 0);
-        native.setPedFleeAttributes(npc, 15, true);
+        native.setPedFleeAttributes(ped, 15, true);
         for (let i = 0; i < 17; i++) {
           native.setRagdollBlockingFlags(ped, i);
         }
         native.taskPlayAnim(ped, 'rcmjosh1', 'idle', 8, 8, -1, 1, 0, false, false, false);
     }, 500)
-  });
+});
